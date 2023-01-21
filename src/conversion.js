2a4306d38d31374a6bcbc5292b98d4d7622d4e33
Emil Anton <emilbstn@gmail.com>
2023-01-20 20:41:02 -0800

    remove console log statements

--- src/conversion.js
+++ src/conversion.js
@@ -6,8 +6,6 @@ export default function getConversion(currency, handleResponse) {
   const url = 'https://v6.exchangerate-api.com/v6/0d88c95aa6b9b351c7b9b8f5/pair/USD/' + currency;
 
   request.addEventListener("loadend", function(){ 
-    console.log("loadend evt listener");
-    console.log(this.responseText);
     const response = JSON.parse(this.responseText)
     if (this.status === 200) {
       handleResponse(response.conversion_rate, currency);
@@ -16,5 +14,3 @@ export default function getConversion(currency, handleResponse) {
   request.open('GET', url);
   request.send();
 }
-
-
