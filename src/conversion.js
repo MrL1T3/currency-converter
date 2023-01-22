f879d661874a7d838e24a7b43b14144d8ae827f5
Emil Anton <emilbstn@gmail.com>
2023-01-21 20:33:46 -0800

    add error status code handling to getConversion

--- src/conversion.js
+++ src/conversion.js
@@ -10,6 +10,21 @@ export default function getConversion(currency, handleResponse) {
     if (this.status === 200) {
       handleResponse(response.conversion_rate, currency);
     }
+    else if (this.status === 400){
+      document.querySelector('#error-zone').innerText = "400 error, bad API request"
+    }
+    else if (this.status === 401){
+      document.querySelector('#error-zone').innerText = "401 error, unauthorized access"
+    }
+    else if (this.status === 403){
+      document.querySelector('#error-zone').innerText = "403 error, forbidden to access"
+    }
+    else if (this.status === 404){
+      document.querySelector('#error-zone').innerText = "400 error, resource cannot be found"
+    }
+    else if (this.status === 500){
+      document.querySelector('#error-zone').innerText = "500 error, internal server issue"
+    }
   });
   request.open('GET', url);
   request.send();
